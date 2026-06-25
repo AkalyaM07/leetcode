33086689ac7eaded3e85a4a757e9f7e60baeaa7b
AkalyaM07 <akalya.m5376@gmail.com>
2026-06-25 10:50:45 +0530

    Add files via upload

---
+++ 169.py
@@ -0,0 +1,26 @@
+"""Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2"""
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        c=0
+        for i in nums:
+            if c==0:
+                can=i
+            if i==can:
+                c+=1
+            else:
+                c-=1
+        return can
